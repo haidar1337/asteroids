aa5331d6145d75a8a68134a464facc23114c9f8d
haidar1337 <haidar.abugurain@gmail.com>
2024-08-25 16:03:03 +0300

    feat: complete player logic

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
+    updateable, drawable = pygame.sprite.Group(), pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    clock = pygame.time.Clock()
+    dt = 0
+    
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        for u in updateable:
+            u.update(dt)
         screen.fill((0,0,0))
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+    
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
